472c20eacc8124f3d61f5b75daf8f0ee14e5c446
Jakub Mamala <G00408184@atu.ie>
2024-09-30 14:14:47 +0100

    updated code before leaving class

--- src/main/java/ie/atu/week3openfeign/Controller.java
+++ src/main/java/ie/atu/week3openfeign/Controller.java
@@ -1,11 +1,23 @@
 package ie.atu.week3openfeign;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/inventory")
 public class Controller {
-
+    @Autowired
+    private Service service;
+    @GetMapping("/GET")
+    public List<Product> getProduct() {
+        return service.getProduct();
+    }
+    @PostMapping("/add")
+    public String addProduct(@RequestBody Product product) {
+        return service.addProduct(product);
+    }
 }
