5ed03cd37ccc2126250be7770c5e2a9ecfc50d96
Jakub Mamala <G00408184@atu.ie>
2024-09-30 13:52:12 +0100

    made product class

---
+++ src/main/java/ie/atu/week3openfeign/Product.java
@@ -0,0 +1,15 @@
+package ie.atu.week3openfeign;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private String id;
+    private String name;
+    public float price;
+}
